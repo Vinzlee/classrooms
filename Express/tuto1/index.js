ca88dc8691590e7c7b4dd4f12afb5491cdfdb79a
Vinzlee <vinz.chollet@gmail.com>
2016-11-27 22:46:43 +0100

    Commit the first running project

---
+++ Express/tuto1/index.js
@@ -0,0 +1,18 @@
+/**
+ * This program belongs to Vincent CHOLLET.
+ * It is considered a trade secret, and is not to be divulged or used
+ * by parties who have not received written authorization from the owner.
+ * For more details please contact us on vinz.chollet@gmail.com
+ *
+ * @author         Vincent CHOLLET
+ * @company        -
+ * @version        1.0
+ * @date           27/11/2016
+ */
+
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+   console.log("listening on 127.0.0.1:3000");
+});
